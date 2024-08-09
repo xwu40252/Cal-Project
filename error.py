d7de9561926cf19d02397d36cf6b72a5e8eb79bc
Xinyi Wu <w13512806006@icloud.com>
2024-08-09 15:18:28 -0700

    error.py

--- error.py
+++ error.py
@@ -123,49 +123,40 @@ file_path1 = 'C:/Users/john/Documents/over.csv'
 file_path2 = 'C:/Users/john/Documents/0.95.csv'
 file_path3 = 'C:/Users/john/Documents/0.7.csv'
 
-# First set of parameters for file 7600.csv
 cycle_length1 = 60
 green_start1 = 33
 green_end1 = 53
-delta_u1 = 4
-phi1 = 0.05
+delta_u1 = 1
+phi1 = 0.2
 
-# Second set of parameters for file 4500.csv
 cycle_length2 = 60
 green_start2 = 33
 green_end2 = 53
-delta_u2 = 4
-phi2 = 0.05
+delta_u2 = 1
+phi2 = 0.2
 
-# Third set of parameters for file 1600.csv
 cycle_length3 = 60
 green_start3 = 33
 green_end3 = 53
-delta_u3 = 4
-phi3 = 0.05
+delta_u3 = 1
+phi3 = 0.2
 
-# Process data for the first file with first set of parameters
 phis1, mpe_values1, min_phi1, min_error1 = process_data(file_path1, cycle_length1, green_start1, green_end1, delta_u1, phi1)
 
-# Process data for the second file with second set of parameters
 phis2, mpe_values2, min_phi2, min_error2 = process_data(file_path2, cycle_length2, green_start2, green_end2, delta_u2, phi2)
 
-# Process data for the second file with second set of parameters
 phis3, mpe_values3, min_phi3, min_error3 = process_data(file_path3, cycle_length3, green_start3, green_end3, delta_u3, phi3)
 
 plt.figure(figsize=(10, 6))
 
-# Plot for the first file and parameters
 plt.plot(phis1, mpe_values1, marker='o', label='7600 vph Percentage Error')
 plt.scatter([min_phi1], [min_error1], color='red', zorder=5)
 plt.text(min_phi1, min_error1, f'Error\nφ={min_phi1:.3f}\nError={min_error1:.4f}', fontsize=9, ha='left')
 
-# Plot for the second file and parameters
 plt.plot(phis2, mpe_values2, marker='s', label='4500 vph Percentage Error')
 plt.scatter([min_phi2], [min_error2], color='blue', zorder=5)
 plt.text(min_phi2, min_error2, f'Error\nφ={min_phi2:.3f}\nError={min_error2:.4f}', fontsize=9, ha='left')
 
-# Plot for the third file and parameters
 plt.plot(phis3, mpe_values3, marker='s', label='1600 vph Percentage Error')
 plt.scatter([min_phi3], [min_error3], color='green', zorder=5)
 plt.text(min_phi3, min_error3, f'Error\nφ={min_phi2:.3f}\nError={min_error3:.4f}', fontsize=9, ha='left')
